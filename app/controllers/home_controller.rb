1ebe55a88434c06ea55dd74dee8faa78f0c1415f
pratuat <shrey.is.shrey@gmail.com>
2012-01-02 17:41:38 +0545

    user model completed

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    resource = "user"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IstockpaintingV10::Application.config.secret_token = '536eedb4c1b0e19455280acfaf2ba75ec38e5eed3192d8e80ccd4891cca72893eb750dbbcddb704a647070a8e8a8322d3ee9871e5c8e92ccf1cdc4f9fb44b7a7'
